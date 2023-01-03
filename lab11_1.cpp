d4aaa3957de61420482101d2835f8c2095d3e919
Entity014 <radlove.012@gmail.com>
2023-01-03 15:22:20 +0700

    lab11_1.cpp completed

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -4,10 +4,10 @@ using namespace std;
 int main(){
 	int count[5] = {}; //Declare array count for counting A,B,C,D,F and initialize all element = 0
 	char grade;
-	int i = 0;
+	int i = 1;
 	cout << "Please input grade of each student (A-F) or input 0 to exit." << endl;
 	do{
-		cout << "Student [" << i + 1 << "]: ";
+		cout << "Student [" << i << "]: ";
 		cin >> grade; //The loop must be terminated when grade = '0'
 		if(grade == 'A') count[0] += 1;
 		else if(grade == 'B') count[1] += 1;
@@ -16,12 +16,13 @@ int main(){
 		else if(grade == 'F') count[4] += 1;
 		else if(grade == '0')
 		{
-			break;
 			i--;
+			break;
 		}
 		else 
 		{
 			cout << "Wrong input. Please input again." << endl;
+			i--;
 		}
 		i++;
 	}while(true);
